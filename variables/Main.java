a37c55a3332837f6ae9728879dd56d7bb5e641a5
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2025-08-18 09:42:39 -0300

    learning variables in java

---
+++ variables/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // int age = 21;
+
+        // double price = 21.5;
+        // char grade = 'A';
+        // boolean isStudent = true;
+        // if (isStudent) {
+        //     System.out.print("You are a student");
+        // }else{
+        //     System.out.print("You are not a student");
+        // }
+
+        String name = "Cauan";
+        System.out.print(name);
+
+    }
+}
