e074fdd452cc151a042338780aa844d2b0584320
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2025-09-11 16:07:48 -0300

    learning string methods

---
+++ string-methods/untitled/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String name = "  Cauan     ";
+
+//        int length = name.length();
+//        char letter = name.charAt(0);
+//        int index = name.indexOf("a");
+//        name = name.toUpperCase();
+//        name = name.toLowerCase();
+//        name = name.trim();
+
+        System.out.println(name);
+    }
+}
