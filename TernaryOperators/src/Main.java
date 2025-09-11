c41b1a3044900cc7d9cd9a9beeb296be91bca4fa
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2025-09-11 17:23:38 -0300

    learning ternary operators

---
+++ TernaryOperators/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        int score = 55;
+//        String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+//        System.out.println(passOrFail);
+
+        int number = 4;
+        String evenOrOdd = (number % 2 == 0) ? "EVEN" : "ODD";
+        System.out.println(evenOrOdd);
+    }
+}
