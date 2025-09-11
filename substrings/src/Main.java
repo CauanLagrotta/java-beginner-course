4a44dc9a46e5334cabc0070c8205aaaa36735745
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2025-09-11 16:31:42 -0300

    learning substring method

---
+++ substrings/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String email;
+        String username;
+        String domain;
+
+        System.out.print("Enter your email: ");
+        email = scanner.nextLine();
+
+        if (email.contains("@")) {
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf("@") + 1);
+
+            System.out.println(domain);
+            System.out.println(username);
+        } else {
+            System.out.println("Emails must contain @");
+        }
+        
+        scanner.close();
+    }
+}
