5dca45c0982758f5109d53f95f5d92096915e6ac
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2025-09-11 21:41:03 -0300

    learning about methods in java

---
+++ Methods/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        happyBirthday("Cauan");
+//        System.out.println(square(3));
+        System.out.println(cube(3));
+    }
+
+    static double cube(double number){
+        return Math.pow(number, 3);
+    };
+
+//    static void happyBirthday(String name){
+//        System.out.printf("Happy birthday %s", name);
+//    }
+//    static double square(double number){
+//        return number * number;
+//    }
+
+}
