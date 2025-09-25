164acec7586797d3c7fa01cce7c059048bda8459
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2025-09-25 14:48:55 -0300

    learning generics

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
   public static void main(String[] args) {
-    
+
+//    Box<String> box = new Box<>();
+//
+//    box.setItem("banana");
+//
+//    System.out.println(box.getItem());
+
+    Product<String, Double> product = new Product<>("Shirt", 21.99);
+    System.out.println(product.getItem());
   }
 }
